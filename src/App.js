ab5718325651d953792333b2fb9a58bc8ca7e911
Bahdana B <bahdana.babkova@gmail.com>
2021-04-16 17:37:39 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { getEmployees, getWorklog } from "./api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    loading: true,
+    employees: [],
+    worklog: []
+  };
+
+  render() {
+    const { loading } = this.state;
+
+    if (loading) {
+      return "Loading...";
+    }
+
+    return <div></div>;
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: false
+    });
+  }
 }
 
 export default App;
